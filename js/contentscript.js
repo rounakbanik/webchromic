15a422e8cd299cc8ef952a998a193d9f09a27905
Rounak Banik <rounakbanik@gmail.com>
2016-08-31 00:37:05 +0530

    Refactor code. Add Sarah's Scribbles.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,42 +1,54 @@
-/*$(".xkcd-mlink").click(function() {
-	alert("Hi");
-	$.get("http://xkcd.com/", function(data) {
-		console.log(data);
-		alert("Loaded");
-	})
-})
-$.ajax({
-			type: "GET",
-			crossDomain: "true",
-			dataType: "jsonp",
-			url: "http://xkcd.com/info.0.json",
-			success: function (responseData, textStatus, jqXHR) {
-        		console.log("in");
-    		},
-    		error: function (responseData, textStatus, errorThrown) {
-        		alert('POST failed.');
-    		}
-		});*/
+/*function JSONParser(url, key) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4) {
+			info = xhr.responseText;
+			info_obj = jQuery.parseJSON(info);
+			img_src = info_obj[key];
+			$(".img-container img").attr("src", img_src);
+		}
+	}
+	xhr.send();
+}*/
 
+function JSONParser(event) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", event.data.url, true);
+	xhr.onreadystatechange = function() {
+  		if (xhr.readyState == 4) {
+    			// innerText does not let the attacker inject HTML elements.
+    		info = xhr.responseText;
+    		info_obj = jQuery.parseJSON(info);
+    		img_src = info_obj[event.data.key];
+    		$(".img-container img").attr("src", img_src);
+  		}
+	}
+	xhr.send();
+}
+
+function HTMLParser(event) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", event.data.url, true);
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4) {
+			info = xhr.responseText;
+			info_obj = jQuery.parseHTML(info);
+			img_src = event.data.prestr + $(info_obj).find(event.data.key).attr("src");
+			$(".img-container img").attr("src", img_src);
+		}
+	}
+	xhr.send();
+
+}
 
 $(document).ready(function() {
-	//Parser for xkcd comics. Thankfully, JSON file directly available
-	$(".xkcd-mlink").click(function() {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "https://xkcd.com/info.0.json", true);
-		xhr.onreadystatechange = function() {
-  			if (xhr.readyState == 4) {
-    			// innerText does not let the attacker inject HTML elements.
-    			info = xhr.responseText;
-    			info_obj = jQuery.parseJSON(info);
-    			img_src = info_obj["img"];
-    			$(".img-container img").attr("src", img_src);
-  			}
-		}
-		xhr.send();
-	});
 
-	$(".cyanide-mlink").click(function() {
+	$(".xkcd-mlink").click({url: "https://xkcd.com/info.0.json", key: "img"}, JSONParser);
+	$(".cyanide-mlink").click({url: "http://explosm.net/comics/latest", key: "#main-comic", prestr: "http:"}, HTMLParser);
+	$(".sarah-mlink").click({url: "http://sarahcandersen.com/", key: "article img", prestr: ""}, HTMLParser);
+
+	/*$(".cyanide-mlink").click(function() {
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", "http://explosm.net/comics/latest", true);
 		xhr.onreadystatechange = function() {
@@ -45,11 +57,25 @@ $(document).ready(function() {
 				info_obj = jQuery.parseHTML(info);
 				img_src = "http:" + $(info_obj).find('#main-comic').attr("src")
 				$(".img-container img").attr("src", img_src);
+			}
+		}
+		xhr.send();
+	});*/
+
+	/*$(".sarah-mlink").click(function() {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "http://sarahcandersen.com/", true);
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4) {
+				info = xhr.responseText;
+				info_obj = jQuery.parseHTML(info);
+				img_src = $(info_obj).find('article img').attr("src")
+				$(".img-container img").attr("src", img_src);
 				console.log(img_src);
 			}
 		}
 		xhr.send();
-	})
+	});*/
 
 
-})
+});
