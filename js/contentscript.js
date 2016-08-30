57bba57962838bc6e563ba9090054d5fb821c616
Rounak Banik <rounakbanik@gmail.com>
2016-08-31 01:09:01 +0530

    Add the initial 10 comics.

--- js/contentscript.js
+++ js/contentscript.js
@@ -47,35 +47,12 @@ $(document).ready(function() {
 	$(".xkcd-mlink").click({url: "https://xkcd.com/info.0.json", key: "img"}, JSONParser);
 	$(".cyanide-mlink").click({url: "http://explosm.net/comics/latest", key: "#main-comic", prestr: "http:"}, HTMLParser);
 	$(".sarah-mlink").click({url: "http://sarahcandersen.com/", key: "article img", prestr: ""}, HTMLParser);
-
-	/*$(".cyanide-mlink").click(function() {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "http://explosm.net/comics/latest", true);
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState == 4) {
-				info = xhr.responseText;
-				info_obj = jQuery.parseHTML(info);
-				img_src = "http:" + $(info_obj).find('#main-comic').attr("src")
-				$(".img-container img").attr("src", img_src);
-			}
-		}
-		xhr.send();
-	});*/
-
-	/*$(".sarah-mlink").click(function() {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "http://sarahcandersen.com/", true);
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState == 4) {
-				info = xhr.responseText;
-				info_obj = jQuery.parseHTML(info);
-				img_src = $(info_obj).find('article img').attr("src")
-				$(".img-container img").attr("src", img_src);
-				console.log(img_src);
-			}
-		}
-		xhr.send();
-	});*/
-
+	$(".smbc-mlink").click({url: "http://www.smbc-comics.com/", key: "#cc-comic", prestr: ""}, HTMLParser);
+	$(".pbs-mlink").click({url: "http://www.gocomics.com/pearlsbeforeswine", key: ".strip", prestr: ""}, HTMLParser);
+	$(".bcf-mlink").click({url: "http://www.buttercupfestival.com/", key: "center img", prestr: ""}, HTMLParser);
+	$(".oglaf-mlink").click({url: "http://oglaf.com/", key: "#strip", prestr: ""}, HTMLParser);
+	$(".girl-mlink").click({url: "http://www.girlgeniusonline.com/comic.php", key: "#comicbody img", prestr: ""}, HTMLParser);
+	$(".yeti-mlink").click({url: "http://theawkwardyeti.com/", key: "#comic img", prestr: ""}, HTMLParser);
+	$(".pdl-mlink").click({url: "http://poorlydrawnlines.com/", key: ".post p img", prestr: ""}, HTMLParser);
 
 });
