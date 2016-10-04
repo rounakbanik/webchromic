65e700ef8b5c67178905b32fe9a93f24d5f764a5
Rounak Banik <rounakbanik@gmail.com>
2016-10-04 20:44:16 +0530

    Add Abstruse Goose.

--- js/contentscript.js
+++ js/contentscript.js
@@ -51,7 +51,10 @@ function HTMLParser(event) {
 			info = xhr.responseText;
 			info_obj = jQuery.parseHTML(info);
 			img_src = event.data.prestr + $(info_obj).find(event.data.key).attr("src");
-			console
+			if(event.data.url == "http://abstrusegoose.com/") {
+				image = $(info_obj).find("img").get(1);
+				img_src = $(image).attr("src");
+			}
 			console.log(img_src)
 			$(".img-container img").fadeOut(400, function() {
             		$(this).attr('src',img_src);
@@ -76,6 +79,10 @@ function HTMLParse(event) {
 			info = xhr.responseText;
 			info_obj = jQuery.parseHTML(info);
 			img_src = event.prestr + $(info_obj).find(event.key).attr("src");
+			if(event.url == "http://abstrusegoose.com/") {
+				image = $(info_obj).find("img").get(1);
+				img_src = $(image).attr("src");
+			}
 			 $(".img-container img").fadeOut(400, function() {
             		$(this).attr('src',img_src);
             		$(".img-container img").css("transform", event.scale);
@@ -92,15 +99,14 @@ websites = [{url: "https://xkcd.com/info.0.json", key: "img", scale: "scale(1.1)
 	{url: "http://sarahcandersen.com/", key: "article img", prestr: "", scale: "scale(0.7)", margin: "-85px"},
 	{url: "http://poorlydrawnlines.com/", key: ".post p img", prestr: "", scale: "scale(0.9)", margin: "0px"},
 	{url: "http://spikedmath.com/", key: ".asset-body center img", prestr: "", scale: "scale(0.8)", margin: "30px"},
-	{url: "http://theawkwardyeti.com/", key: "#comic img", prestr: "", scale: "scale(0.4)", margin: "-430px"},
+	{url: "http://theawkwardyeti.com/", key: "#comic img", prestr: "", scale: "scale(0.4)", margin: "-230px"},
 	{url: "http://www.girlgeniusonline.com/comic.php", key: "#comicbody img", prestr: "", scale: "scale(0.6)", margin: "-205px"},
 	{url: "http://oglaf.com/", key: "#strip", prestr: "", scale: "scale(0.9)", margin: "-5px"},
 	{url: "http://carbon-comic.com/", key: "#comic img", prestr: "", scale: "scale(0.8)", margin: "20px"},
 	{url: "http://www.buttercupfestival.com/", key: "center img", prestr: "", scale: "scale(0.95)", margin: "35px"},
+	{url: "http://abstrusegoose.com/", key: "section img", prestr: "", scale: "scale(0.7)", margin:"30px"},
 	{url: "http://thisisindexed.com/", key: "#content div:nth-child(2) .entry-content p img", prestr: "", scale: "scale(1.3)", margin: "80px"},
-	{url: "http://brownsharpie.courtneygibbons.org/", key: "#comic-1 a img", prestr: "", scale: "scale(0.7)", margin: "30px"},
 	{url: "http://www.incidentalcomics.com/", key: ".blog-posts .date-outer:first-child .date-posts .post-outer .post-body .separator a img", prestr: "",scale: "scale(0.9)", margin: "10px"},
-	{url: "http://explosm.net/comics/latest", key: "#main-comic", prestr: "http:", scale: "scale(0.75)", margin: "-55px"}
 ];
 
 $(document).ready(function() {
@@ -118,14 +124,13 @@ $(document).ready(function() {
 	$(".xkcd-mlink").click({url: "https://xkcd.com/info.0.json", key: "img", scale: "scale(1.1)", margin: "85px"}, JSONParser);
 	$(".sarah-mlink").click({url: "http://sarahcandersen.com/", key: "article img", prestr: "", scale: "scale(0.7)", margin: "-85px"}, HTMLParser);
 	$(".pdl-mlink").click({url: "http://poorlydrawnlines.com/", key: ".post p img", prestr: "", scale: "scale(0.9)", margin: "0px"}, HTMLParser);
-	$(".yeti-mlink").click({url: "http://theawkwardyeti.com/", key: "#comic img", prestr: "", scale: "scale(0.4)", margin: "-430px"}, HTMLParser);
+	$(".yeti-mlink").click({url: "http://theawkwardyeti.com/", key: "#comic img", prestr: "", scale: "scale(0.4)", margin: "-230px"}, HTMLParser);
 	$(".girl-mlink").click({url: "http://www.girlgeniusonline.com/comic.php", key: "#comicbody img", prestr: "", scale: "scale(0.6)", margin: "-205px"}, HTMLParser);
 	$(".oglaf-mlink").click({url: "http://oglaf.com/", key: "#strip", prestr: "", scale: "scale(0.9)", margin: "-5px"}, HTMLParser);
 	$(".bcf-mlink").click({url: "http://www.buttercupfestival.com/", key: "center img", prestr: "", scale: "scale(0.95)", margin: "35px"}, HTMLParser);
-	$(".cyanide-mlink").click({url: "http://explosm.net/comics/latest", key: "#main-comic", prestr: "http:", scale: "scale(0.75)", margin: "-55px"}, HTMLParser);
 	$(".spiked-mlink").click({url: "http://spikedmath.com/", key: ".asset-body center img", prestr: "", scale: "scale(0.8)", margin: "30px"}, HTMLParser);
-	$(".sharpie-mlink").click({url: "http://brownsharpie.courtneygibbons.org/", key: "#comic-1 a img", prestr: "", scale: "scale(0.7)", margin: "30px"}, HTMLParser);
 	$(".incidental-mlink").click({url: "http://www.incidentalcomics.com/", key: ".blog-posts .date-outer:first-child .date-posts .post-outer .post-body .separator a img", prestr: "", scale: "scale(0.9)", margin: "10px"}, HTMLParser);
 	$(".carbon-mlink").click({url: "http://carbon-comic.com/", key: "#comic img", prestr: "", scale: "scale(0.8)", margin: "20px"}, HTMLParser);
 	$(".indexed-mlink").click({url: "http://thisisindexed.com/", key: "#content div:nth-child(2) .entry-content p img", prestr: "", scale: "scale(1.3)", margin: "80px"}, HTMLParser);
+	$(".abstruse-mlink").click({url: "http://abstrusegoose.com/", key: "section img", prestr: "", scale: "scale(0.7)", margin:"30px"}, HTMLParser);
 });
